fdcb7d040fec992d7e16162ffe94903e17213e0a
jung <2323hsb@gmail.com>
2019-03-05 20:32:06 +0900

    new ccc

--- westory/main/urls.py
+++ westory/main/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
+    path('stories', views.StoriesView.as_view()),
     path('posts', views.PostsView.as_view()),
     path('conditions', views.ConditionsView.as_view()),
+    path('test', views.CreateStoryView.as_view()),
     # path('post', views.PostListView.as_view()),
     # path('create', views.CreateView.as_view()),
 ]
--- westory/main/views.py
+++ westory/main/views.py
@@ -1,7 +1,13 @@
 from django.views.generic import TemplateView
 
+class StoriesView(TemplateView):
+    template_name = 'main/stories.html'
+
 class PostsView(TemplateView):
     template_name = 'main/posts.html'
 
 class ConditionsView(TemplateView):
     template_name = 'main/conditions.html'
+
+class CreateStoryView(TemplateView):
+    template_name = 'main/create_story.html'
